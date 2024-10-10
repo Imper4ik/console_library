4722180e6f2c377b9ff4e4a182307d797bc64599
Butouski Mikita <nindse@gmail.com>
2024-10-10 16:03:57 +0200

    Добавлены основные файлы проекта

---
+++ main.py
@@ -0,0 +1,168 @@
+import sql_db
+
+
+class UserRegistration:
+    def __init__(self):
+        self.name = ""
+        self.surname = ""
+        self.age = 0
+        self.username = ""
+        self.password = ""
+
+    @staticmethod
+    def get_valid_name(prompt):
+        while True:
+            name = input(prompt).lower()
+            if name.isalpha():
+                return name.capitalize()
+            else:
+                print("Пожалуйста, введите корректное имя.")
+
+    @staticmethod
+    def get_valid_age():
+        while True:
+            try:
+                age = int(input("Введите ваш возраст: "))
+                if 0 < age < 120:
+                    return age
+                else:
+                    print("Пожалуйста, введите корректный возраст от 1 до 120.")
+            except ValueError:
+                print("Пожалуйста, введите корректное число.")
+
+    def register(self):
+        self.name = self.get_valid_name("Введите ваше имя: ")
+        self.surname = self.get_valid_name("Введите вашу фамилию: ")
+        self.age = self.get_valid_age()
+        self.username = input("Введите ваше имя пользователя: ")
+        self.password = input("Введите ваш пароль: ")
+
+        # Проверка наличия пользователя
+        if sql_db.check_user_exists(self.username):
+            print("Имя пользователя уже существует. Пожалуйста, выберите другое имя.")
+            return False
+
+        check_password = input('Введите ваш пароль еще раз: ')
+        while True:
+            if self.password == check_password:
+                print('Пароль подтвержден.')
+                sql_db.insert_customer(self.name, self.surname, self.age, self.username, self.password)  # Добавляем пользователя в БД
+                return True  # Возвращаем True при успешной регистрации
+            else:
+                print('Пароли не совпадают, попробуйте еще раз.')
+
+
+class UserLogin:
+    def __init__(self, connection):
+        self.user = ""
+        self.password = ""
+        self.connection = connection  # Сохраняем соединение
+
+    def authorize(self):
+        while True:
+            self.user = input("Введите ваше имя пользователя: ")
+            self.password = input("Введите ваш пароль: ")
+            # Передаем соединение в check_user_credentials
+            if sql_db.check_user_credentials(self.connection, self.user, self.password):
+                print("Авторизация успешна!")
+                return True  # Успешный вход
+            else:
+                print("Неверное имя пользователя или пароль. Пожалуйста, попробуйте снова.")
+
+
+def display_menu():
+    menu = [
+        '1 - Регистрация',
+        '2 - Вход',
+        '3 - Добавить книгу',
+        '4 - Удалить книгу',
+        '5 - Показать ваши книги',
+        '6 - Показать все книги',
+        '7 - Выход'
+    ]
+
+    print("\n[Меню]:")
+    for option in menu:
+        print(option)
+
+
+def main():
+    interface = True
+    is_logged_in = False  # Флаг для проверки, вошел ли пользователь в систему
+    login = None  # Сохраняем информацию о входе пользователя
+
+    try:
+        connection = sql_db.connect_db()  # Устанавливаем соединение
+        connection.autocommit = True
+
+        while interface:
+            display_menu()
+
+            try:
+                choice = int(input('\nВаш выбор: '))
+                if choice == 1:
+                    registration = UserRegistration()
+                    if registration.register():  # Проверка успешной регистрации
+                        print(f"Привет {registration.username}! Вы успешно зарегистрированы.")
+                elif choice == 2:
+                    login = UserLogin(connection)  # Передаем соединение
+                    if login.authorize():  # Проверка успешной авторизации
+                        print(f"Добро пожаловать {login.user}!")
+                        is_logged_in = True  # Устанавливаем флаг, если пользователь вошел
+
+                elif choice == 3:
+                    if is_logged_in:
+                        name_of_book = input("Введите название книги: ")
+                        author_of_book = input("Введите автора книги: ")
+                        year_of_book = input("Введите год издания книги: ")
+                        user_id = sql_db.check_user_login(connection, login.user)  # Получаем ID пользователя
+                        sql_db.add_book(connection, name_of_book, author_of_book, year_of_book,
+                                        user_id)  # Передаем ID пользователя
+                        print(f"Книга '{name_of_book}' авторства {author_of_book} успешно добавлена.")
+                    else:
+                        print("Вы должны войти в систему, чтобы добавить книги.")
+
+                elif choice == 4:
+                    if is_logged_in:
+                        book_id = input("Введите ID книги для удаления: ")
+                        sql_db.remove_book(connection, book_id)  # Передаем соединение и ID книги
+                    else:
+                        print("Вы должны войти в систему, чтобы удалить книги.")
+
+
+                elif choice == 5:
+                    if is_logged_in:
+                        user_id = sql_db.check_user_login(connection, login.user)  # Получаем ID пользователя
+                        user_books = sql_db.get_user_books(connection, user_id)  # Передаем ID для получения книг
+                        if user_books:  # Если книги найдены
+                            print("Ваши книги:")
+                            for book in user_books:
+                                print(book)
+                        else:
+                            print("У вас еще нет добавленных книг.")
+
+                elif choice == 6:
+                    if is_logged_in:
+                        sql_db.show_all_books(connection)  # Передаем соединение
+                    else:
+                        print("Вы должны войти в систему, чтобы просмотреть все книги.")
+                elif choice == 7:
+                    print("До свидания!")
+                    interface = False
+                else:
+                    print("Пожалуйста, введите корректный вариант.")
+
+            except ValueError:
+                print("Неверный ввод. Пожалуйста, введите число от 1 до 7.")
+
+    except Exception as ex:
+        print('[ERROR] Ошибка при работе с PostgreSQL:', ex)
+
+    finally:
+        if connection:
+            connection.close()
+            print('[INFO] Соединение с PostgreSQL закрыто.')
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_db.py
@@ -0,0 +1,150 @@
+import psycopg2
+import bcrypt
+from config import host, user, password, dbname
+
+# Функция для подключения к PostgreSQL
+
+
+def connect_db():
+    return psycopg2.connect(database=dbname, user=user, password=password, host=host)
+
+
+def create_tables(connection):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS LIBRARY(
+            id SERIAL PRIMARY KEY,
+            name_the_book VARCHAR(255) NOT NULL,
+            author_the_book VARCHAR(255) NOT NULL,
+            year_the_book INTEGER NOT NULL
+            );
+            """
+        )
+        print('[INFO] Таблица LIBRARY создана')
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Customer_info(
+            id SERIAL PRIMARY KEY,
+            name_the_customer VARCHAR(255) NOT NULL,
+            surname_the_customer VARCHAR(255) NOT NULL,
+            age_the_customer INT NOT NULL,
+            username_the_customer VARCHAR(255) NOT NULL UNIQUE,
+            password_the_customer VARCHAR(255) NOT NULL
+            );
+            """
+        )
+        print('[INFO] Таблица Customer_info создана')
+
+
+def check_user_login(connection, username):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT id FROM Customer_info WHERE username_the_customer = %s;",
+            (username,)
+        )
+        user_id = cursor.fetchone()
+        if user_id:
+            # Если пользователь существует, возвращаем его ID
+            return user_id[0]
+        return None
+
+
+def check_user_exists(username):
+    with connect_db() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT 1 FROM Customer_info WHERE username_the_customer = %s;",
+                (username,)
+            )
+            return cursor.fetchone() is not None
+
+
+def check_user_credentials(connection, username, password):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT password_the_customer FROM Customer_info WHERE username_the_customer = %s;",
+            (username,)
+        )
+        hashed_password = cursor.fetchone()
+        if hashed_password and bcrypt.checkpw(password.encode('utf-8'), hashed_password[0].encode('utf-8')):
+            return True
+        return False  # Возвращает False, если пользователь не найден или пароль неверный
+
+
+def insert_customer(name, surname, age, username, password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    with connect_db() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO Customer_info(name_the_customer, surname_the_customer, age_the_customer, username_the_customer, password_the_customer)
+                VALUES (%s, %s, %s, %s, %s);
+                """,
+                (name, surname, age, username, hashed_password)
+            )
+            print('[INFO] Данные пользователя успешно добавлены!')
+
+
+def add_book(connection, name_of_book, author_of_book, year_of_book, customer_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            INSERT INTO User_Books (name_the_book, author_the_book, year_the_book, customer_id)
+            VALUES (%s, %s, %s, %s);
+            """,
+            (name_of_book, author_of_book, year_of_book, customer_id)
+        )
+        print(f'[INFO] Книга "{name_of_book}" авторства {author_of_book} добавлена в вашу библиотеку.')
+
+
+def remove_book(connection, book_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            DELETE FROM User_Books 
+            WHERE id = %s;  -- Убедитесь, что у вас есть поле id в таблице
+            """,
+            (book_id,)
+        )
+        print(f'[INFO] Книга с ID {book_id} удалена из вашей библиотеки.')
+
+
+def show_all_books(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT name_the_book, author_the_book, year_the_book FROM LIBRARY;")
+        books = cursor.fetchall()
+        print(f'[INFO] Книги в библиотеке:')
+        for book in books:
+            print(book)
+
+
+def get_user_books(connection, user_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT name_the_book, author_the_book, year_the_book 
+            FROM User_Books 
+            WHERE customer_id = %s;  -- Используйте ID пользователя
+            """,
+            (user_id,)
+        )
+        return cursor.fetchall()
+
+
+def main():
+    try:
+        connection = connect_db()
+        connection.autocommit = True
+        create_tables(connection)  # Создаем таблицы, если их еще нет
+    except Exception as ex:
+        print('[ERROR] Ошибка при работе с PostgreSQL:', ex)
+    finally:
+        if connection:
+            connection.close()
+            print('[INFO] Соединение с PostgreSQL закрыто.')
+
+
+if __name__ == "__main__":
+    main()
